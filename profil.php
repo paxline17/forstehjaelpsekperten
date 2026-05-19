0c470cfc95c9e8b72f4f68952110a9224005a831
Line <lil006@edu.zealand.dk>
2026-05-19 11:24:31 +0200

    Navbar + nye farver indsat

---
+++ profil.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @var db $db
+ */
+
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Førstehjælpseksperten</title>
+    <link rel="icon" type="" href="">
+
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <script src="https://kit.fontawesome.com/5458120b39.js" crossorigin="anonymous"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body>
+
+
+
+<?php
+include("includes/navbar.php" );
+?>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+
+</body>
+</html>
