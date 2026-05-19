11bca68874855eccf31d8ae95768949b105efef4
Line <lil006@edu.zealand.dk>
2026-05-19 16:52:21 +0200

    Profil (Igang med profilindstillinger)

--- profil.php
+++ profil.php
@@ -28,6 +28,41 @@ require "settings/init.php";
 
 <body>
 
+<div class=" profile-page container py-4">
+    <div class="text-center mb-4 pt-4">
+        <div class="mx-auto mb-4">
+            <i class="fa-solid fa-user fa-2xl"></i>
+        </div>
+        <h5 class="fw-bold">Emil Østergaard</h5>
+        <p class="text-muted small">Emil.oest@mail.dk | +45 52 40 18 39</p>
+    </div>
+</div>
+
+<div class="card profile-card mx-4 mt-5">
+    <div class="card-header text-center fw-bold py-3">
+        Profilindstillinger
+    </div>
+
+    <ul class="list-group list-group-flush">
+
+        <button class="list-group-item list-group-item-action d-flex justify-content-between align-items-center py-3"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#profileSubMenu"
+        aria-expanded="false"
+        aria-controls="profileSubMenu">
+        <div>Ændre profil</div>
+            <i class="fa-solid fa-arrow-right"></i>
+        </button>
+
+        <div></div>
+
+    </ul>
+
+</div>
+
+
+
 
 
 <?php
