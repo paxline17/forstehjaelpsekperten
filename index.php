d9f21fc3cfb9d3c3f135d86380e1cdc66c2c6921
zoemoulder <zoemoulder@gmail.com>
2026-05-21 10:40:16 +0200

    guide boxes

--- index.php
+++ index.php
@@ -17,14 +17,39 @@ require "settings/init.php";
     <meta name="copyright" content="Information om copyright">
     
     <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <script src="https://kit.fontawesome.com/5458120b39.js" crossorigin="anonymous"></script>
     
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
 
-<h1>Det virker 🥳</h1>
+<div class="container">
+
+<p class="text-center mb-3">Akutte guides</p>
+
+<div class="row g-3 mb-4">
+
+<div class="col-6">
+    <a href="#hjertestop" class="text-decoration-none text-dark text-center border border-dark d-block bg-white">
+        <i class="fa-solid fa-heart-pulse"></i>
+        <p class="fw-bold mt-2 text-dark">Hjertestop</p>
+    </a>
+</div>
+
+<div class="col-6">
+          <a href="#kvælning" class="text-decoration-none text-dark text-center border border-dark d-block bg-white">
+        <i class="fa-solid fa-head-side-cough"></i>
+    <p class="fw-bold mt-2 text-dark">Kvælning</p>
+          </a>
+</div>
+</div>
+</div>
+
+</body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
