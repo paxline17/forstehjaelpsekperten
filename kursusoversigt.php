24df1a249d605e770a734ab3f625ef0c1c7dc76c
zoemoulder <zoemoulder@gmail.com>
2026-05-22 12:45:49 +0200

    fixing structure

--- kursusoversigt.php
+++ kursusoversigt.php
@@ -7,7 +7,7 @@
 require "settings/init.php";
 ?>
 <!DOCTYPE html>
-<div lang="da">
+<html lang="da">
 <head>
     <meta charset="utf-8">
 
@@ -17,12 +17,17 @@ require "settings/init.php";
     <meta name="author" content="Udgiver">
     <meta name="copyright" content="Information om copyright">
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
     <script src="https://kit.fontawesome.com/5458120b39.js" crossorigin="anonymous"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
+
+<body>
+
 
 <div class="container">
     <h1 class="text-center fw-bold mb-2 fs-5">Kursusoversigtigt</h1>
@@ -108,11 +113,6 @@ require "settings/init.php";
 
 </div>
 </div>
-
-<body>
-
-
-
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
