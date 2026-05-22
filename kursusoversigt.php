6b804213360664d1a85746f69f37535c107e98e3
zoemoulder <zoemoulder@gmail.com>
2026-05-22 12:29:32 +0200

    classes

--- kursusoversigt.php
+++ kursusoversigt.php
@@ -7,7 +7,7 @@
 require "settings/init.php";
 ?>
 <!DOCTYPE html>
-<html lang="da">
+<div lang="da">
 <head>
     <meta charset="utf-8">
 
@@ -19,6 +19,8 @@ require "settings/init.php";
 
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
+    <script src="https://kit.fontawesome.com/5458120b39.js" crossorigin="anonymous"></script>
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
@@ -28,10 +30,22 @@ require "settings/init.php";
     <a href="#" class="text-center mb-4 text-decoration-none text-dark">[Læs mere om tilmelding her]</a>
     </p>
 
+<div class="row g-3">
+    <div class="col-12 col-lg-4">
+        <div class="border border-dark p-3">
+            <p class="text-center mb-2">Basal førstehjælp til børn</p>
+            <p class="small">Sætter deltagerne i stand til at handle ved hjertestop og andre livstruende hændelser, der involverer børn....</p>
+            <p class="small"><i class="fa-solid fa-clock"></i>Varighed: 240 minutter</p>
+        </div>
+        <a href="#" class="border border-top-0 border-dark p-2 d-flex justify-content-between text-dark">
+             Tilmeld <i class="fa-solid fa-arrow-right"></i>
+    </a>
+    </div>
+
+
+
 </div>
-
-
-
+</div>
 
 <body>
 
