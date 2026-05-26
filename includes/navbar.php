adcf9c732ac153aa727e1d335bfb9c98c1416b1b
Line <lil006@edu.zealand.dk>
2026-05-26 09:20:58 +0200

    kursusoversigt (tilført grå til kasserne)

--- includes/navbar.php
+++ includes/navbar.php
@@ -4,25 +4,25 @@
 
         <div class="d-flex justify-content-around p-2 w-100">
 
-            <a href="" class="text-reset">
+            <a href="index.php" class="text-reset">
                 <i class="fa-solid fa-house fa-xl"></i>
             </a>
 
 
-            <a href="#" class="text-reset">
+            <a href="guides.php" class="text-reset">
                 <i class="fa-solid fa-book-open fa-xl"></i>
             </a>
 
 
-            <a href="" class="text-reset">
+            <a href="kursusoversigt.php" class="text-reset">
                 <i class="fa-solid fa-graduation-cap fa-xl"></i>
             </a>
 
-            <a href="" class="text-reset">
+            <a href="Games.php" class="text-reset">
                 <i class="fa-solid fa-gamepad fa-xl"></i>
             </a>
 
-            <a href="" class="text-reset">
+            <a href="profil.php" class="text-reset">
                 <i class="fa-solid fa-circle-user fa-xl"></i>
             </a>
         </div>
